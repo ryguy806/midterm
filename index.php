fb1f426fa8b7da7108eec6cd4dffe83428c64bb2
Ryan Guelzo <rguelzo@mail.greenriver.edu>
2019-05-13 16:42:47 -0400

    added the summary page and updated the index.

--- index.php
+++ index.php
@@ -19,12 +19,30 @@ $f3->route('GET /', function (){
 $f3->route('GET|POST /survey', function ($f3){
 
     $options = array('This midterm is easy', 'I like midterms', 'Detective Pikachu was great!');
-
     $f3->set('options', $options);
 
-    //default route
+    if(!empty($_POST)){
+
+        $name = $_POST['name'];
+        $selections = $_POST['options'];
+
+        $f3->set('name', $name);
+        $f3->set('selections', $selections);
+
+        $_SESSION['name'] = $f3->get('name');
+        $_SESSION['selections'] = implode(", " ,$f3->get('selections'));
+
+        $f3->reroute('/summary');
+    }
+
     $view = new Template();
     echo $view->render('views/form1.html');
+});
+
+$f3->route('GET|POST /summary', function (){
+
+    $view = new Template();
+    echo $view->render('views/results.html');
 });
 
 $f3->run();
